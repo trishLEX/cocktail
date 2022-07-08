67924914fc113515ae42f1c7c6994a84fd145b9b
trishLEX <alexeytr5@gmail.com>
2022-07-08 23:52:48 +0300

    add get ingredients api

---
+++ generated/src/main/kotlin/org/openapitools/model/IngredientLightDTO.kt
@@ -0,0 +1,25 @@
+package org.openapitools.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+
+/**
+ * 
+ * @param id 
+ * @param preview 
+ * @param name 
+ */
+data class IngredientLightDTO(
+
+    @Schema(example = "null", description = "")
+    @field:JsonProperty("id") val id: kotlin.Int? = null,
+
+    @Schema(example = "null", description = "")
+    @field:JsonProperty("preview") val preview: kotlin.ByteArray? = null,
+
+    @Schema(example = "null", description = "")
+    @field:JsonProperty("name") val name: kotlin.String? = null
+) {
+
+}
+
